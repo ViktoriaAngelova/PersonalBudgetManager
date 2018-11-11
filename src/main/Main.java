d23d0453c32c5d5eef9e2debee99e43e382edafb
ViktoriaAngelova <viktoria.angelova0311@gmail.com>
2018-11-11 22:22:59 +0200

    Login System created
    Login system with single user with verification of username and
    password.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,37 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
+	public static Scanner scanner = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Testing issues
+
+		String username = "Admin";
+		String password = "1234";
+
+		System.out.println("Welcome to Personal Budget Manager!");
+		System.out.println("USER: ");
+		String usernameInput = scanner.nextLine();
+
+		// Verification on the input data for user name
+		while (!usernameInput.equals(username)) {
+			System.out.println("Invalid username!");
+			System.out.println("USER: ");
+			usernameInput = scanner.nextLine();
+		}
+
+		System.out.println("PASSWORD: ");
+		String passwordInput = scanner.nextLine();
+
+		// Verification on the input data for password
+		while (!passwordInput.equals(password)) {
+			System.out.println("Invalid password!");
+			System.out.println("PASSWORD: ");
+			passwordInput = scanner.nextLine();
+		}
+		
 	}
 
 }
