5a35004f70052e8bb3d99c29f455b29c90058c76
ViktoriaAngelova <viktoria.angelova0311@gmail.com>
2018-11-14 11:34:11 +0200

    New revenue functionality implemented
    Added option for creating new revenues with validation on the input data
    format

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,16 +1,19 @@
 package main;
 
 import java.util.Scanner;
+import java.util.regex.*;
 
 public class Main {
 
 	public static Scanner scanner = new Scanner(System.in);
+	public static String choice;
+	public static StringBuilder dateBuilder = new StringBuilder();
+	public static StringBuilder valueBuilder = new StringBuilder();
 
 	public static void main(String[] args) {
 
 		String username = "Admin";
 		String password = "1234";
-		String choice;
 
 		System.out.println("Welcome to Personal Budget Manager!");
 		System.out.println("USER: ");
@@ -34,6 +37,13 @@ public class Main {
 		}
 
 		System.out.println("Hello, " + usernameInput + "!");
+		startMenu();
+
+		scanner.close();
+	}
+
+	public static void startMenu() {
+
 		System.out.println("For new revenue press R.");
 		System.out.println("For new cost press C.");
 		System.out.println("To display a balance press B: ");
@@ -43,16 +53,64 @@ public class Main {
 			choice = scanner.nextLine().toLowerCase();
 			switch (choice) {
 			case "r":
+				newRevenue();
 				break;
 			case "c":
+				// TODO
 				break;
 			case "b":
+				// TODO
 				break;
 			default:
 				System.out.println("Invalid choice! Please, press R, C or B, depending on your choice: ");
 			}
 		} while (choice != "r" || choice != "c" || choice != "b");
 
+	}
+
+	public static void newRevenue() {
+
+		System.out.println("-------New Revenue-------");
+		System.out.println("[add date (dd.mm.yyyy)] $ [add value (0,00)]");
+
+		String inputRevenue = scanner.nextLine();
+		String validRevenue = dataValidation(inputRevenue);
+		// TODO processData(validRevenue);
+
+		System.out.println("To add another revenue press A.");
+		System.out.println("To go back to start menu press M: ");
+
+		// Validating user input
+		do {
+			choice = scanner.nextLine().toLowerCase();
+			switch (choice) {
+			case "a":
+				newRevenue();
+				break;
+			case "m":
+				startMenu();
+				break;
+			default:
+				System.out.println("Invalid choice! Please, press A or M, depending on your choice: ");
+			}
+		} while (choice != "a" || choice != "m");
+
+	}
+
+	public static String dataValidation(String data) {
+
+		// Setting date and value format
+		String regex = "^(\\d{2}\\.\\d{2}\\.\\d{4})(\\s\\$\\s)(\\d{1,}\\,\\d{2})$";
+		boolean isMatching = Pattern.matches(regex, data);
+
+		// Date and value validation
+		while (!isMatching) {
+			System.out.println("Invalid date or value format! Enter date and value in the correct format: ");
+			data = scanner.nextLine();
+			isMatching = Pattern.matches(regex, data);
+		}
+
+		return data;
 	}
 
 }
